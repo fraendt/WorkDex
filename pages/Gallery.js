5a42ce86a0126aea7bf6141ea2d003324bc056c5
Patrick Zhang <zhangpatrick2004@gmail.com>
2021-10-20 23:10:53 -0400

    unfinished work finish tomorrow

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import UserContext from '../components/UserContext';
 import { View, Text, Image } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 
 
@@ -9,13 +10,19 @@ const Gallery = ({ navigation }) => {
   const { cachedImages, setCachedImages } = useContext(UserContext);
   console.log(cachedImages);
 
-  return (
+  if (0) return (
     <View>
       {cachedImages.map((val, idx) => {return (
         // <Text key={idx}>{val.base64}</Text>
         <Image source={val} key={idx}/>
       )})}
     </View>
+  )
+
+  return (
+    <Swipeable>
+      
+    </Swipeable>
   )
 }
 
