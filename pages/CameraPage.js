00d6672d8ead368f53103bad2abdc7bc696aa73c
Patrick Zhang <zhangpatrick2004@gmail.com>
2021-10-20 22:59:16 -0400

    replaced icons and inlined css

--- pages/CameraPage.js
+++ pages/CameraPage.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, TouchableNativeFeedback, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableNativeFeedback, Image, TouchableHighlight } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useIsFocused } from '@react-navigation/native';
 // import Icon from 'react-native-vector-icons/Ionicons';
 //import Icon from 'react-native-vector-icons/MaterialIcons';
-import {MaterialIcons, Ionicons} from 'react-native-vector-icons';
+import {MaterialIcons, Ionicons, Entypo, MaterialCommunityIcons} from 'react-native-vector-icons';
 import { AutoFocus } from 'expo-camera/build/Camera.types';
 import UserContext from '../components/UserContext';
 
@@ -30,43 +30,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     width: '100%',
     flexDirection: 'column',
-  },
-  snap: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    borderWidth: 0,
-  },
-  flip: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    borderWidth: 0,
-  },
-  gallery: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    borderWidth: 0,
-  },
-  snapIcon: {
-    position: 'absolute',
-    fontSize: 80,
-    bottom: 30,
-    left: "50%",
-    marginLeft: -40,
-  },
-  flipIcon: {
-    position: 'absolute',
-    fontSize: 80,
-    top: '5%',
-    left: 30,
-  },
-  galleryIcon: {
-    position: 'absolute',
-    fontSize: 80,
-    bottom: 30,
-    right: 0,
   },
 });
 
@@ -102,15 +65,26 @@ const CameraPage = ({ navigation }) => {
     <View style={styles.container}>
       { isFocused && 
       <Camera 
-        style={styles.camera} 
+        style={{
+          backgroundColor: 'blue',
+          width: '100%',
+          //height: '100%',
+        }} 
         type={type} 
         ref={ref => {
           setCamera(ref);
         }}
       >
         <View style={styles.buttonContainer}>
-          <TouchableNativeFeedback
-            style={styles.flip}
+          <TouchableHighlight
+            style={{
+              width: 80,
+              height: 80,
+              position: 'absolute',
+              fontSize: 80,
+              top: '5%',
+              left: 30,
+            }}
             onPress={() => {
               setType(
                 type === Camera.Constants.Type.back
@@ -118,21 +92,56 @@ const CameraPage = ({ navigation }) => {
                   : Camera.Constants.Type.back
               );
             }}>
-            <Ionicons name='camera-reverse-outline' color='#fff' style={styles.flipIcon}/>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback
-            style={styles.snap}
+            <Ionicons 
+              name='camera-reverse-outline' 
+              color='#fff' 
+              style={{
+                fontSize: 80,
+              }}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{
+              width: 80,
+              height: 80,
+              position: 'absolute',
+              fontSize: 80,
+              bottom: 30,
+              left: "50%",
+              marginLeft: -40,
+            }}
             onPress={() => {takePicture()}}>
-            <Ionicons name='md-scan-circle-outline' color='#fff' style={styles.snapIcon}/>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback
-            style={styles.gallery}
+            <Entypo //Ionicons 
+              //name='md-scan-circle-outline'
+              name='circle'
+              color='#fff' 
+              style={{
+                fontSize: 80,
+              }}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={{
+              width: 80,
+              height: 80,
+              position: 'absolute',
+              fontSize: 80,
+              bottom: 30,
+              right: 0,
+            }}
             onPress={() => navigation.navigate('Gallery')}
           >
           
-            <MaterialIcons name='photo-album' color='#fff' style={styles.galleryIcon}/>
+            <MaterialCommunityIcons //MaterialIcons 
+              //name='photo-album' 
+              name='image-outline'
+              color='#fff' 
+              style={{
+                fontSize: 80,
+              }}
+            />
 
-          </TouchableNativeFeedback>
+          </TouchableHighlight>
         </View>
       </Camera>
     }
